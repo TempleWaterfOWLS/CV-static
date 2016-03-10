ab256007374763165662b58ebe10821331c003ac
Claire <claire.durand@temple.edu>
2016-03-09 18:35:33 -0800

    loop through input photos to find Canny contours

--- auto_canny.py
+++ auto_canny.py
@@ -1,60 +1,72 @@
 #!/usr/local/bin/python
 
+# lol do I even still use all of these imports?
 import os
 import cv2
 import math
 import numpy as np
 import argparse
 import glob
-
-blank = np.zeros((480, 640), np.uint8)
-blank[:,:] = (255)
+import sys
 
 
-## Resize with resize command
-def resizeImage(img):
+## Set folder of input and output images
+infolder = "input-photos/"
+outfolder = "output-photos-canny/"
+
+"""
+findBuoys(input_image_name)
+Function to use Canny Edge Detection + findContours methods to find buoys in every image in the
+input photo folder, then save the images with the detected contours in the output photo folder.
+It uses weird statistics magic that I stole from the internet to set the parameter for Canny ED.
+""" 
+def findBuoys(imgname):
     
-    # mapping from destination to source
-    dst = cv2.resize(img,None,fx=0.25,fy=0.25,interpolation=cv2.INTER_LINEAR)
-    return dst
+    ## Load image from input photo folder
+    img = cv2.imread(infolder + imgname)
 
-## Load image
-#img = cv2.imread("./buoy_red_1.jpeg")
-img = cv2.imread("./pool.jpg")
+    # Set some parameters automatically (based on statistics magic that I stole from the internet)
+    sigma=0.33
+    med = np.median(img)
+    lower = int(max(0, (1.0 - sigma) * med))
+    upper = int(min(255, (1.0 + sigma) * med))
+    edged = cv2.Canny(img, lower, upper)
 
-# Set some parameters
-sigma=0.33
-med = np.median(img)
-lower = int(max(0, (1.0 - sigma) * med))
-upper = int(min(255, (1.0 + sigma) * med))
-edged = cv2.Canny(img, lower, upper)
+    ## Make image single stream black and white
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-## Make image single stream black and white
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-can = cv2.Canny(gray, 25, 75)
+    ## Not gonna lie, I don't super remember what this does
+    filteredContour = []
 
-filteredContour = []
+    ## Find the contours in the image composed of Canny edges
+    im2, contour,hier = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for iteration in range(0,len(contour)):
+        areaOfContour = cv2.contourArea(contour[iteration])
+        if areaOfContour >= 200:
+            print "Contour %d of size %d" %(iteration,areaOfContour)
+            filteredContour.append(contour[iteration])
+            cv2.drawContours(img, filteredContour, -1, (0,255,0), 2)
 
-im2, contour,hier = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-for iteration in range(0,len(contour)):
+    ## Save Images with Canny blobs in output photo folder
+    stg = outfolder + imgname
+    cv2.imwrite(stg, img)
 
-    areaOfContour = cv2.contourArea(contour[iteration])
-    if areaOfContour >= 500:
-        print "Contour %d of size %d" %(iteration,areaOfContour)
-        filteredContour.append(contour[iteration])
-        cv2.drawContours(img, filteredContour, -1, (0,255,0), 2)
+    ## End of findBuoys
 
+
+## Actual main function that I would write cleanly if I wasn't terrible:
     
-#blank = cv2.dilate(blank,kernel, iterations=1)
-cv2.imshow("canny", can)
-cv2.imshow("colorCanny", edged)
+## Iterate through all of the input jpg photos in the directory under the working dir
+## and run the findBuoys Canny function on them.
+for i in os.listdir(os.getcwd()+"/input-photos/"):
+    if i.endswith(".jpg"):
+        print i
+        findBuoys(i)
+        continue
+    else:
+        continue
 
-## Show Images
-cv2.imshow("gray", gray)
-cv2.imshow("img",img)
 
-
-while(1):
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        break
+## Close OpenCV and exit Python script when done
+cv2.destroyAllWindows()
+sys.exit(0)
