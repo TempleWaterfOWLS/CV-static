a8b0a4fe125a79e935b9221febbed2376686b326
ecdoyle <edoyle@temple.edu>
2016-04-11 19:57:21 -0400

    inc

--- navLog.py
+++ navLog.py
@@ -5,12 +5,20 @@ import os
 import os.path
 import canny
 import canny_single
-import subprocess
+import numpy
 
 def main():
-    m=subprocess.Popen("canny_single.py",shell=True)
+    # read in the integer values which represent the binary of where the buoys
+    # are in the matrix through the use of canny_single and providing the
+    # image to be checked
+    int_values=canny_single.canny_single("left0002.jpg")
+    
+    # convert the 4th row into 8 bit binary to determine precise local of buoys
+    m= '{0:08b}'.format(int_values[4])
 
-    #input argument is currently just the row of the matrix (start at 5th one)
+    # input argument is currently just the row of the matrix (start at 5th one)
+    # starting at middle, check to find where 2 spots are open and determine
+    # the relative command that should be sent
     if m[3] and m[4]:
         command="forward"
     elif m[3] and m[2]:
@@ -28,8 +36,9 @@ def main():
     else:
         command="stop"
     print (command)
+    return command
 
-#publish the command to the topic
+# then publish the command to the topic
     
 if __name__=="__main__":
     main()
